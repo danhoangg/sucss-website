93c57f37eb5885c01697cab59040a70e780b8b4a
Daniel Hoang <hoang.daniel2005@gmail.com>
2024-07-11 01:27:07 +0100

    production: website ready for production

--- backend/app.py
+++ backend/app.py
@@ -327,6 +327,6 @@ def get_docs(docs):
         return jsonify({'error': f'An error occurred: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    #from waitress import serve
-    #serve(app, host="0.0.0.0", port=8080)
-    app.run(debug=True)
+    #app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
